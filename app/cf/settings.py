7f5bde8d27d143e34bac242db500bd8dbdced54f
Swelan R. Auguste <swelanauguste@gmail.com>
2023-03-24 18:19:09 -0400

    settings

--- app/cf/settings.py
+++ app/cf/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "request.middleware.RequestMiddleware",
 ]
 
 ROOT_URLCONF = "cf.urls"
@@ -67,7 +68,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "request.middleware.RequestMiddleware",
             ],
         },
     },
